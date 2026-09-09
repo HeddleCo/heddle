02bc400f94500b44fc4b8d71e124b7121ea04e70
Zephyr <zephyr@inskape.xyz>
2026-09-09 03:07:51 +0200

    chore: consume typed State content contract

--- crates/hosted-client/src/hosted_runtime/hosted/helpers.rs
+++ crates/hosted-client/src/hosted_runtime/hosted/helpers.rs
@@ -6,7 +6,6 @@ use api::heddle::api::v1alpha1::{
     RepositoryRef, StateAttachmentKind as ProtoStateAttachmentKind, StateId as ProtoStateId,
     TransferCheckpoint, TransportMode, repository_ref::Reference,
 };
-use base64::Engine as _;
 use config::ClientConfig;
 use objects::object::{ContentHash, StateAttachmentId, StateAttachmentKind, StateId};
 use wire::{ObjectId, ObjectInfo, ObjectType, ProtocolError};
@@ -118,19 +117,6 @@ pub(super) fn parse_descriptor_to_info(
         size: 0,
         delta_base: None,
     })
-}
-
-pub(super) fn decode_blob_content(
-    content: String,
-    is_binary: bool,
-) -> Result<Vec<u8>, ProtocolError> {
-    if is_binary {
-        base64::engine::general_purpose::STANDARD
-            .decode(content.as_bytes())
-            .map_err(|err| ProtocolError::Serialization(err.to_string()))
-    } else {
-        Ok(content.into_bytes())
-    }
 }
 
 pub(super) fn parse_object_id(
@@ -808,17 +794,6 @@ mod tests {
         let mut bad = to_proto_object_info(&attachment_info);
         bad.attachment_kind = ProtoStateAttachmentKind::Unspecified as i32;
         assert!(parse_descriptor_to_info(bad).is_err());
-    }
-
-    #[test]
-    fn decode_blob_content_handles_text_and_base64() {
-        assert_eq!(
-            decode_blob_content("hello".into(), false).unwrap(),
-            b"hello"
-        );
-        let encoded = base64::engine::general_purpose::STANDARD.encode(b"\0\x01\x02");
-        assert_eq!(decode_blob_content(encoded, true).unwrap(), vec![0, 1, 2]);
-        assert!(decode_blob_content("!!!".into(), true).is_err());
     }
 
     #[test]
